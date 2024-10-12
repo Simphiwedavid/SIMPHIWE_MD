5670f618749e2b6943ab2ebc4b86ac6e2e3d56f7
Simphiwedavid <simphiwetheking2022@gmail.com>
2024-10-12 10:42:05 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,81 @@
+//SIMPHIWE_MD
+
+const fs = require('fs')
+const { color } = require('./lib/myfunc')
+
+//owner
+global.owner = '27747815326, 27648920467'
+global.nomerowner = ["27747815326, 27648920467"]
+
+// Apikey 
+
+global.skizo = 'KATAKURI, SIMPHIWE'
+global.casterix = 'Icon_Simphiwe'
+//watermark 
+global.packname = '**'
+global.author = 'SIMPHIWE_MD'
+
+// cpanel 
+global.domain = 'https://' // Icon_Simphiwe
+global.apikey2 = 'ptlc' // Simphiwe at your service
+global.capikey2 = 'ptla' // Simphiwe x David
+global.eggsnya = '15' // The id eggs used if the id is 5, just leave it, don't change it
+global.location = '1' // id location
+
+
+global.apilinode = ''// apikey vps account linode
+global.apitokendo = ''
+
+//database 
+global.urldb = ''; // just leave it blank but if you want to use the mongo database, fill in the mongo url
+
+global.limitawal = {
+    premium: "Infinity",
+    monayawal: 0,
+    free: 100
+}
+
+//rpg
+global.buruan = {
+   ikan: 5,
+   ayam: 5,
+   kelinci: 5,
+   domba: 5,
+   sapi: 5,
+   gajah: 5
+}
+global.rpg = {
+   darahawal: 100,
+   energyawal: 252,
+   besiawal: 5,
+   goldawal: 1,
+   emeraldawal: 1,
+   umpanawal: 1,
+   potionawal: 1
+}
+
+//auto functioner
+global.autoTyping = false;                //make true to enable auto typing
+global.autoRecord = false;                //make true to enable auto recording
+global.statusseen = false;                 //make true to view statuses
+global.unavailable = true;     
+//to show your real presence  
+global.available = false;   
+// to show always online  
+global.autoreadmessages = false;   
+///To always read messages
+global.chatbot = false;
+global.autoreact = false;
+
+global.autobio = false;
+//_____________//
+// Don't change it
+global.antibot = false
+//—————「 Deadline 」—————//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(color(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
